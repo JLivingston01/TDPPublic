70126f960bb0cac8b0b4ac6dd9d9ca7d331108b3
JLivingston01 <33710575+JLivingston01@users.noreply.github.com>
2022-09-19 21:20:46 -0400

    minor cleanup 6

--- tdp_modules/network/_email.py
+++ tdp_modules/network/_email.py
@@ -16,7 +16,14 @@ class EmailClient:
         self.server=None
 
     def __authenticate__(self):
+        """
+        Authentication method for email client.
 
+        Returns
+        -------
+        SELF.
+
+        """
         context = ssl.create_default_context()
 
         self.server = smtplib.SMTP_SSL("smtp.gmail.com",
@@ -30,6 +37,21 @@ class EmailClient:
                      message,
                      receivers
                      ):
+        """
+        Send message to receivers from client.
+
+        Parameters
+        ----------
+        message : Str
+            Body and subject of message.
+        receivers : List or Str
+            Recipient email addresses.
+
+        Returns
+        -------
+        None.
+
+        """
 
         self.__authenticate__()
         self.server.sendmail(self.sender, receivers, message)
